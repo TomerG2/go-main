4c119cff96bc3a2f3a5506a10dbd44d774769d2b
tomer <tomer.g2@gmail.com>
2025-02-23 18:36:39 +0200

    add missing package

--- models/parser.go
+++ models/parser.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"fmt"
+	url2 "net/url"
+	"strings"
+)
+
+func ParseLines(lines []string) (escapedUrls []string) {
+	for _, line := range lines {
+		escapedUrls = append(escapedUrls, parseLine(line))
+	}
+	return escapedUrls
+}
+
+func parseLine(url string) (escapedUrl string) {
+	parts := strings.Split(url, "=")
+	escapedUrl = fmt.Sprintf("%s=%s", parts[0], url2.QueryEscape(parts[1]))
+	return escapedUrl
+}
