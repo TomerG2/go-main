36a4b035de3d2e2ca72cfd7bb035cd83124a2c77
tomer <tomer.g2@gmail.com>
2025-02-23 18:37:58 +0200

    add missing package

--- models/reader_test.go
+++ models/reader_test.go
@@ -1,1 +1,13 @@
 package models
+
+import (
+	"testing"
+	"tomerg2/go-main/conf"
+)
+
+func TestParseFile(t *testing.T) {
+	res := ReadFile(conf.QueryList)
+	if len(res) != 5 {
+		t.Fatalf("Missing expected results")
+	}
+}
