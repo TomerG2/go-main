4565a093a1b8ed28ffd3e082ded3541d60ff498a
tomer <tomer.g2@gmail.com>
2025-02-23 18:37:14 +0200

    add missing package

--- models/reader.go
+++ models/reader.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadFile(path string) []string {
+	var queries []string
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		queries = append(queries, line)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error reading file: %s", err)
+	}
+	return queries
+}
