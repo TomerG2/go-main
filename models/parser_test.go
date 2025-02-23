4721c98e2673d7da2b0e1a2030b3e3c448d1cbe2
tomer <tomer.g2@gmail.com>
2025-02-23 18:36:56 +0200

    add missing package

--- models/parser_test.go
+++ models/parser_test.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"testing"
+)
+
+func TestParseLines(t *testing.T) {
+	exp := "https://api.github.com/search/code?q=class+in%3Afile+language%3Ajava+repo%3Ascala%2Fscala"
+	lines := []string{
+		"https://api.github.com/search/code?q=class in:file language:java repo:scala/scala",
+	}
+	res := ParseLines(lines)
+	if len(res) != 1 {
+		t.Fatalf("Wrong number of urls")
+	}
+	if res[0] != exp {
+		t.Fatalf("URLs parsed incorrectly")
+	}
+}
