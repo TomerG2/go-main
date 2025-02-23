caafa39fa5902ff09f376d70212319080cd88824
tomer <tomer.g2@gmail.com>
2025-02-23 18:40:30 +0200

    improve tests

--- models/reader_test.go
+++ models/reader_test.go
@@ -6,8 +6,12 @@ import (
 )
 
 func TestParseFile(t *testing.T) {
+	exp := "https://api.github.com/search/code?q=class in:file language:java repo:scala/scala"
 	res := ReadFile(conf.QueryList)
 	if len(res) != 5 {
 		t.Fatalf("Missing expected results")
+	}
+	if res[0] != exp {
+		t.Fatalf("Wrong lines")
 	}
 }
