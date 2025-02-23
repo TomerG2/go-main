f9c46b718a6ef607c88226445aa9cdb29f668f9f
tomer <tomer.g2@gmail.com>
2025-02-23 18:34:42 +0200

    add main

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package go_main
+package main
+
+import (
+	"fmt"
+	"tomerg2/go-main/conf"
+	"tomerg2/go-main/models"
+)
+
+func main() {
+	fmt.Printf("Reading input file\n")
+	lines := models.ReadFile(conf.QueryList)
+	fmt.Printf("Reading input file [lines=%d]\n", len(lines))
+
+	fmt.Printf("Escaping URLs\n")
+	urls := models.ParseLines(lines)
+	fmt.Printf("Escaping URLs [lines=%d]\n", len(urls))
+
+	//fmt.Printf("Querying URLs\n")
+	//models.QueryURLs(urls)
+	//fmt.Printf("Querying URLs\n")
+
+	fmt.Printf("Querying URLs\n")
+	models.QueryURLsPaginated(urls)
+	fmt.Printf("Querying URLs\n")
+}
